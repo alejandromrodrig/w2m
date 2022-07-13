c862dd3cd9b2b5c141f230d32f4b56c065d61f02
Alejandro <amilan@hiberus.com>
2022-07-13 09:01:35 +0200

    Add more tests II

--- src/test/java/com/example/demo/controller/HeroControllerTest.java
+++ src/test/java/com/example/demo/controller/HeroControllerTest.java
@@ -1,8 +1,15 @@
 package com.example.demo.controller;
 
+import static com.example.demo.entity.Category.C;
+import static com.example.demo.entity.Gender.M;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.example.demo.entity.Hero;
 import org.apache.http.auth.AuthScope;
@@ -16,7 +23,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -35,7 +48,7 @@ class HeroControllerTest {
   }
 
   private static HttpComponentsClientHttpRequestFactory getClientHttpRequestFactory() {
-    HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
+    final HttpComponentsClientHttpRequestFactory clientHttpRequestFactory
         = new HttpComponentsClientHttpRequestFactory();
 
     clientHttpRequestFactory.setHttpClient(httpClient());
@@ -44,12 +57,12 @@ class HeroControllerTest {
   }
 
   private static HttpClient httpClient() {
-    CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+    final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
 
     credentialsProvider.setCredentials(AuthScope.ANY,
         new UsernamePasswordCredentials("admin", "admin"));
 
-    HttpClient client = HttpClientBuilder
+    final HttpClient client = HttpClientBuilder
         .create()
         .setDefaultCredentialsProvider(credentialsProvider)
         .build();
@@ -62,12 +75,56 @@ class HeroControllerTest {
   }
 
   @Test
-  public void returnsAHero() {
-    Hero heroResponse = restTemplate.getForObject(baseUrl.concat("/{id}"), Hero.class, 1);
+  public void getsAHeroById() {
+    final Hero heroResponse = restTemplate.getForObject(baseUrl.concat("/{id}"), Hero.class, 1);
     assertAll(
         () -> assertNotNull(heroResponse),
         () -> assertEquals("Spiderman", heroResponse.getName())
     );
   }
 
+  @Test
+  public void findsAHeroByName() {
+    final List<Hero> heroResponse = restTemplate.getForObject(baseUrl.concat("/name/{keywords}"), List.class, "Man");
+    assertAll(
+        () -> assertNotNull(heroResponse),
+        () -> assertEquals(4, heroResponse.size())
+    );
+  }
+
+  @Test
+  public void createsAHero() {
+    final Hero hero = new Hero("Capitan Garfio", M, "662500221", "Capitan", C);
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    final HttpEntity<Hero> httpEntity = new HttpEntity<>(hero, headers);
+
+    final Hero heroResponse = restTemplate.postForObject(baseUrl, httpEntity, Hero.class);
+    assertAll(
+        () -> assertNotNull(heroResponse),
+        () -> assertNotNull(heroResponse.getId()),
+        () -> assertEquals("Capitan Garfio", heroResponse.getName()));
+  }
+
+  @Test
+  public void updatesAHero() {
+    final Hero hero = restTemplate.getForObject(baseUrl.concat("/{id}"), Hero.class, 1);
+    hero.setName("Nuevo Heroe");
+    final HttpEntity<Hero> entity = new HttpEntity<Hero>(hero);
+    final ResponseEntity heroResponse = restTemplate.exchange(baseUrl, HttpMethod.PUT, entity, Hero.class);
+    final Hero heroUpdated = (Hero) heroResponse.getBody();
+    assertAll(
+        () -> assertNotNull(heroUpdated),
+        () -> assertNotNull(heroUpdated.getId()),
+        () -> assertEquals("Nuevo Heroe", heroUpdated.getName()));
+  }
+
+  @Test
+  public void deletesAHeroById() {
+    final Map<String, String> params = new HashMap<String, String>();
+    params.put("id", "1");
+    restTemplate.delete(baseUrl.concat("/{id}"), params);
+    assertThrows(HttpClientErrorException.class, () -> restTemplate.getForObject(baseUrl.concat("/{id}"), Hero.class, 1));
+
+  }
 }
